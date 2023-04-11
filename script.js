d9b56b4df576f1d6bda55c153bcc75bb1ae80db5
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-11 16:47:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
 
-  return ;//return decodedArr
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
